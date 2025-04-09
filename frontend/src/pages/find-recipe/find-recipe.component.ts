b8a9e3b1ce020ebfab7798146f950e88da8a827a
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-04-09 09:24:36 +0200

    Add some componentfor pages + some thoughts on design

---
+++ frontend/src/pages/find-recipe/find-recipe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-recipe',
+  imports: [],
+  templateUrl: './find-recipe.component.html',
+  styleUrl: './find-recipe.component.css'
+})
+export class FindRecipeComponent {
+
+}
